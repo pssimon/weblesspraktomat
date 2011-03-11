345a317558b3e1950d21c34476efb259f0defeeb
Martin Rombach <martin.rombach@web.de>
2011-03-11 19:52:29 +0100

    merge command, zip-download ...

--- weblesspraktomat.pl
+++ weblesspraktomat.pl
@@ -72,92 +72,89 @@ $start_vpn && system($vpn_start_command);
 
 # Check argument and act accordingly
 my $arg = $ARGV[0] || '';
+my $task = $ARGV[1] || '';
+$task =~ /^\d+$/ || die("Invalid task :-(\n");
 if ( $arg eq 'get' ) {
-    download_testing($mech);
+    download_testing( $mech, $task );
 }
 elsif ( $arg eq 'put' ) {
-    commit($mech);
+    commit( $mech, $task );
+}
+elsif ( $arg eq 'merge' ) {
+	merge_single_files( $task );
 }
 elsif ( $arg eq 'start' ) {
-    my $task = $ARGV[1] || '';
-    $task =~ /^\d+$/ || die("Invalid task :-(\n");
     for my $num (@numbers) {
         start_testing( $mech, $num, $task );
     }
 }
 else {
-    print "valid options: get, put, start [number]\n";
+    print "valid options: get, put, start, merge [number]\n";
 }
 $start_vpn && system($vpn_stop_command);
 
 #This will submit everything in the Data dir, it relies on the file structure, so don't mess with that.
 sub commit {
     my $mech = shift;
+    my $task = shift;
     $mech->get($praktomat_base);
     my @working =
       grep { $_->text() =~ /in Bearbeitung/ }
-      grep { $_->url()  =~ /OpenDocumentPage/ } $mech->find_all_links();
+      grep { $_->url()  =~ /OpenDocumentPage/ }
+      grep { $_->url()  =~ /task=$task/ }
+      		$mech->find_all_links();
     for my $studdir (<$datadir/*>) {
-        $studdir =~ m{.*/(.+)$};
-        my $matnr = $1;
-        for my $taskdir (<$studdir/*>) {
-            $taskdir =~ m{.*/(.+)$};
-            my $task           = $1;
-            my $textfeld       = slurp( $taskdir . "/_textfeld.txt" );
-            my $bewertungsfeld = slurp( $taskdir . "/_bewertungsfeld.txt" );
-            my $aufdenweggeben = slurp( $taskdir . "/_aufdenweggeben.txt" );
-            my $punktezahl     = slurp( $taskdir . "/_punktezahl.txt" );
-            my ($link) = grep { $_->url() =~ m{review/$matnr/$task} } @working;
-            die unless $link;
-            $mech->get( $link->url() );
-            $mech->form_with_fields( "solution", "annotated_solution" );
-            $mech->set_fields(
-                "annotated_solution" => $textfeld,
-                "comment_rating"     => $bewertungsfeld,
-                "comment_general"    => $aufdenweggeben,
-                "review_comment"     => $punktezahl,
-            );
-            $mech->click_button( name => "PreviewJudgmentPage" );
-            $verbose && print "Uploading Mat: $matnr Aufg: $task\n";
-        }
+        my ($matnr) = $studdir =~ m{.*/(.+)$};
+       	my $taskdir = $studdir . "/" . $task;
+       	-d $taskdir || next;
+        my $textfeld       = slurp( $taskdir . "/_textfeld.txt" );
+        my $bewertungsfeld = slurp( $taskdir . "/_bewertungsfeld.txt" );
+        my $aufdenweggeben = slurp( $taskdir . "/_aufdenweggeben.txt" );
+        my $punktezahl     = slurp( $taskdir . "/_punktezahl.txt" );
+        my ($link) = grep { $_->url() =~ m{review/$matnr/$task} } @working;
+        die unless $link;
+        $mech->get( $link->url() );
+        $mech->form_with_fields( "solution", "annotated_solution" );
+        $mech->set_fields(
+            "annotated_solution" => $textfeld,
+            "comment_rating"     => $bewertungsfeld,
+            "comment_general"    => $aufdenweggeben,
+            "review_comment"     => $punktezahl,
+        );
+        $mech->click_button( name => "PreviewJudgmentPage" );
+        $verbose && print "Uploading Mat: $matnr Aufg: $task\n";
     }
 }
 
 # Download everything you've got marked as testing
 sub download_testing {
     my $mech = shift;
+    my $task = shift;
     $mech->get($praktomat_base);
     my @working =
       grep { $_->text() =~ /in Bearbeitung/ }
-      grep { $_->url()  =~ /OpenDocumentPage/ } $mech->find_all_links();
+      grep { $_->url()  =~ /OpenDocumentPage/ }
+      grep { $_->url()  =~ /task=$task/ }
+      		$mech->find_all_links();
     for my $link (@working) {
-        $link->url() =~ m{review/(\d+)/(\d+)/};
+        $link->url() =~ m{review/(\d+)/$task/};
         my $matnr = $1;
-        my $task  = $2;
         $verbose && print "Getting   Mat: $matnr Aufg: $task\n";
         $mech->get($link);
         mkdir $datadir . "/" . $matnr;
-        mkdir $datadir . "/" . $matnr . "/" . $task;
+        my $workingdir = $datadir . "/" . $matnr . "/" . $task . "/";
+        mkdir $workingdir;
         my $form = $mech->form_with_fields( "solution", "annotated_solution" );
-        my ($textarea) =
-          grep { $_->name() eq 'annotated_solution' } @{ $form->{inputs} };
-        my @list = content_split( $textarea->{value} );
 
-        for my $file (@list) {
-            open( my $output, ">:utf8",
-                $datadir . "/" . $matnr . "/" . $task . "/" . $file->[0] )
-              || die $!;
-            print $output $file->[1];
-            close $output;
-        }
-        field_to_file( "annotated_solution", $form,
-            $datadir . "/" . $matnr . "/" . $task . "/_textfeld.txt" );
-        field_to_file( "comment_rating", $form,
-            $datadir . "/" . $matnr . "/" . $task . "/_bewertungsfeld.txt" );
-        field_to_file( "comment_general", $form,
-            $datadir . "/" . $matnr . "/" . $task . "/_aufdenweggeben.txt" );
-        field_to_file( "review_comment", $form,
-            $datadir . "/" . $matnr . "/" . $task . "/_punktezahl.txt" );
+		$mech->follow_link( text => "Quelltext herunterladen" );
+		$mech->save_content( $workingdir . "solution.zip" );
+		$mech->back();
+		system( "unzip -qo -d ${workingdir}src/ ${workingdir}solution.zip" );
+        
+        field_to_file( "annotated_solution", $form, $workingdir . "_textfeld.txt" );
+        field_to_file( "comment_rating", $form, $workingdir . "_bewertungsfeld.txt" );
+        field_to_file( "comment_general", $form, $workingdir . "_aufdenweggeben.txt" );
+        field_to_file( "review_comment", $form, $workingdir . "_punktezahl.txt" );
         $mech->back();
     }
 }
@@ -213,10 +210,38 @@ sub content_split {
     return @out;
 }
 
+# merge single files in the _textfeld.txt file
+sub merge_single_files {
+	my $task = shift;
+    
+    for my $studdir (<$datadir/*>) {
+        my $taskdir = "$studdir/$task/";
+        -d $taskdir || next;
+        
+		open( my $out, ">", $taskdir . "_textfeld.txt" ) || die $!;
+		for my $file (<${taskdir}src/*.java>) {
+			$file =~ m{([^/]+)$};
+			my $filename = $1;
+		
+			open( my $in, "<", $file ) || die $! . " $file\n";
+			print $out "\t=== $filename ===\n";
+			while( <$in> ) {
+				/^p[?|!]/ || print $out "\t";
+				# every file must end with a newline
+				chomp;
+				print $out "$_\n";
+			}					
+			print $out "\t--- $filename ---\n";	
+			close( $in );	
+		}
+		close( $out );
+	}
+}
+
 # Read in a whole file in one gulp
 sub slurp {
     my $filename = shift;
-    open( my $in, "<:utf8", $filename ) || die $! . " $filename\n";
+    open( my $in, "<", $filename ) || die $! . " $filename\n";
     my $cont = do { local $/; <$in> };
     close $in;
     return $cont;
